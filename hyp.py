e32c385777ef630d11f086881e344b158eb03927
Harsh Vardhan Singh <harsh23.007@gmail.com>
2023-01-30 11:50:48 +0530

    first commit

---
+++ hyp.py
@@ -0,0 +1,8 @@
+per=12
+base=5
+hyp=(per**2+base**2)**.5
+print("PYTHAGOREAS THEOREM")
+print(f"Perpendicular is {per}")
+print(f"Base is {base}")
+print('--'*10)
+print("Hypotenuse is",hyp)
---
+++ si.py
@@ -0,0 +1,10 @@
+# SI Calculator
+p=10000
+r=.15
+t=100
+s=p*r*t/100
+print(f'Principle is ${p}')
+print(f'Rate is {r}%')
+print(f'Time is {t} yrs')
+print('---'*10)
+print(f'Simple Interest is {s}')
